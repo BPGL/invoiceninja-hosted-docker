f0bbe8d05aec9204e8e0014bab704ef971ee4319
Zer0Divis0r <sych@sych.tech>
2023-12-08 21:23:52 +0100

    DB backup script fix

--- _utils/backup_db.sh
+++ _utils/backup_db.sh
@@ -12,8 +12,9 @@ source .env
 current_date=$(date +"%Y-%m-%d")
 current_datetime=$(date +"%Y-%m-%d-%H-%M-%S")
 
-cd /tmp
 docker compose exec -t db bash -c "mysqldump -u$DB_USERNAME -p'$DB_PASSWORD' --no-tablespaces $DB_DATABASE" > /tmp/$current_datetime.sql
+
+cd /tmp
 
 tar -czf /tmp/$current_datetime-db.tar.gz $current_datetime.sql
 
