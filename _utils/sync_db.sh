b92fdf7923e4bbedb8f2be25ccfbd25da200c283
Zer0Divis0r <sych@sych.tech>
2023-12-10 14:32:09 +0100

    Tweaks to github actions

--- _utils/sync_db.sh
+++ _utils/sync_db.sh
@@ -4,11 +4,13 @@ STAGING_DIR=$2
 echo "Exporting from production..."
 cd $PROD_DIR || exit 1
 source ./env
+echo "Exporing database named $DB_DATABASE..."
 docker compose exec -t db bash -c "mysqldump -u$DB_USERNAME -p'$DB_PASSWORD' --no-tablespaces $DB_DATABASE" > /tmp/prod_db.sql
 
 echo "Importing to staging..."
 cd $STAGING_DIR || exit 1
 source ./env
+echo "Importing to database named $DB_DATABASE..."
 docker compose exec -t db bash -c "mysql -u$DB_USERNAME -p'$DB_PASSWORD' -D $DB_DATABASE" < /tmp/prod_db.sql
 
 echo "Removing dump"
